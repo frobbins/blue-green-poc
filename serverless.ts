bab1923fe99f11c1f97d230b987bdc5b5b7fc765
george.robbins <george.robbins@coxautoinc.com>
2023-07-27 02:51:01 -0400

    added deploy stage

--- serverless.ts
+++ serverless.ts
@@ -86,30 +86,30 @@ const serverlessConfiguration: AWS = {
       //     Name: 'live',
       //   },
       // },
-      // GetSwingsAliasStaging: {
-      //   Type: 'AWS::Lambda::Alias',
-      //   Properties: {
-      //     FunctionName: 'wgt-guru-pink-getAllSwings',
-      //     FunctionVersion: '$LATEST',
-      //     Name: 'staging',
-      //   },
-      // },
-      // CreateSwingAliasStaging: {
-      //   Type: 'AWS::Lambda::Alias',
-      //   Properties: {
-      //     FunctionName: 'wgt-guru-pink-createSwing',
-      //     FunctionVersion: '$LATEST',
-      //     Name: 'staging',
-      //   },
-      // },
-      // HealthCheckAliasStaging: {
-      //   Type: 'AWS::Lambda::Alias',
-      //   Properties: {
-      //     FunctionName: 'wgt-guru-pink-healthcheck',
-      //     FunctionVersion: '$LATEST',
-      //     Name: 'staging',
-      //   },
-      // },
+      GetSwingsAliasStaging: {
+        Type: 'AWS::Lambda::Alias',
+        Properties: {
+          FunctionName: 'wgt-guru-pink-getAllSwings',
+          FunctionVersion: '$LATEST',
+          Name: 'staging',
+        },
+      },
+      CreateSwingAliasStaging: {
+        Type: 'AWS::Lambda::Alias',
+        Properties: {
+          FunctionName: 'wgt-guru-pink-createSwing',
+          FunctionVersion: '$LATEST',
+          Name: 'staging',
+        },
+      },
+      HealthCheckAliasStaging: {
+        Type: 'AWS::Lambda::Alias',
+        Properties: {
+          FunctionName: 'wgt-guru-pink-healthcheck',
+          FunctionVersion: '$LATEST',
+          Name: 'staging',
+        },
+      },
     },
   },
 };
